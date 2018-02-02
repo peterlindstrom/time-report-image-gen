d904104f7ccd4d044d75ebcfba7abefe0060810b
peterlindstrom <peter.lindstrom@jayway.com>
2018-02-02 14:55:10 -0800

    using webshot to generate screenshot

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
 # time-report-image-gen
-# Generate image API
-Using jimp to return a image based on text input
+# Generate image out of json => html API
+Using webshot to return a image based on text input
+
+##Setup
+> npm install
+
+##Run
+> node start
+
+##API
+POST http://localhost:3001/createImage
+
+example body: {
+  "table": [
+    {
+      "date": "2018-01-01",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-02",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-03",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-04",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-05",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-06",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    },
+    {
+      "date": "2018-01-07",
+      "hours": 8,
+      "projectName": "TajmShit",
+      "activityName": "Drink beer"
+    }
+  ],
+  "title": "Week 1"
+}
+
+return body: {
+    "outname": "outfile1391ca30-0869-11e8-b1e7-9584e89e8098.jpg"
+}
+
+GET http://localhost:3001/image/{outname} returns the jpg
+
+
--- server.js
+++ server.js
@@ -1,15 +1,140 @@
 const express = require('express')
 const app = express()
 const uuidv1 = require('uuid/v1');
+const queryString = require('query-string'); 
+const tableify = require('tableify');
+const bodyParser = require('body-parser');
+const webshot = require('webshot');
+const multiline = require('multiline');
+const moment = require('moment');
+const _isEmpty = require('lodash.isempty')
+
 var Jimp = require("jimp");
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const customCSS = multiline(()=>{/*
+  *,
+  *:before,
+  *:after {
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+  }
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1024px;
+    height: 768px;
+  } 
+  body {
+    font-family: Open Sans, sans-serif;
+    font-size: 13px;
+    margin: 20px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #000;
+    background-color: transparent; color: #fff;
+    background-size: cover;
+    background-image: url("https://i.imgur.com/kL7S2sK.jpg");
+    width: 100%;
+    height: 100%;
+  }
+
+  h1 {
+    font-size: 21px;
+    margin: 1.5em 0;
+  }
+
+  table {
+    overflow: hidden;
+    width: auto;
+    max-width: 100%;
+    margin: 0 auto;
+    border-collapse: collapse;
+    border-spacing: 0;
+    background-size: cover;
+    background-image: url("https://i.imgur.com/kL7S2sK.jpg");
+  }
+  table td,
+  table th {
+    padding: 10px;
+    position: relative;
+    outline: 0;
+    text-align: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    vertical-align: top;
+    cursor: default;
+  }
+  table th {
+    font-weight: bold;
+  }
+  table thead th {
+    border-bottom-width: 2px;
+  }
+  table tbody th {
+    text-align: left;
+    white-space: nowrap;
+  }
+  table tbody > tr:hover td,
+  table tbody > tr:hover th {
+    background: #fffe96;
+  }
+  table td:hover:after,
+  table thead th:not(:empty):hover:after {
+    content: '';
+    position: absolute;
+    z-index: -2;
+    top: -5000px;
+    left: 0;
+    width: 100%;
+    height: 10000px;
+    background: #fffe96;
+  }
+  table tbody > tr > td:hover {
+    background: #fffd2d;
+  }
+*/});
+
 app.use(express.static(__dirname + '/public'));
     
+app.post('/createImage', jsonParser, (req, res) => {
+  const { body } = req;
+  const { table=[], title='' } = body;
+  if (!_isEmpty(table)) {
+    var html = tableify({
+      [title]: table, 
+    });
+    var outName = `outfile${uuidv1()}.jpg`;
+    var outfile = `${__dirname}/public/${outName}`;
+    var options = {
+      siteType: 'html',
+      customCSS: customCSS,
+    }
+    var fileName = 'http://i.imgur.com/kL7S2sK.jpg';
+    const imgTag = `<img src=${fileName}/>`;
+    const date = table.find((item) => !!item.date);
+    var weeknumber = moment(date.date, "MMDDYYYY").isoWeek();
+    webshot(`<html><body>${imgTag}${html}</body></html>`, outfile, options, function(err) {
+      console.log('err', err);
+      res.json({ outname: outName });
+    });
+  }
+}),  
+app.get('/image/:imageId', (req, res) => {
+  var fileName = req.params.imageId;
+  var outFileName = `${__dirname}/public/${fileName}`;
+  res.sendFile(outFileName, (e) => {
+    console.log('e', e);
+  });
+});  
 app.get('/background', (req, res) => {
   var fileName = __dirname + '/timereg_bg.jpg';
   var imageCaption = 'Week 3';
   var loadedImage;
   var outfile = `${__dirname}/public/outfile${uuidv1()}.jpg`;
-  
+  const query = req.query;
   Jimp.read(fileName)
       .then(function (image) {
           loadedImage = image;
@@ -28,4 +153,4 @@ app.get('/background', (req, res) => {
       });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
